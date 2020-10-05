a888d0b9c68ca9f8e36151a0c9659903667a81ea
Marcin Iwanicki <miwanicki1@bloomberg.net>
2020-10-05 21:13:27 +0100

    Update configure.sh

--- Scripts/configure.sh
+++ Scripts/configure.sh
@@ -15,19 +15,25 @@ function main {
     set -e
 
     # 0.35.0
-    SWIFTLINT_FORMULA="Scripts/Formulas/swiftlint.rb"
+    SWIFTLINT_FORMULA="swiftlint.rb"
 
     # 0.40.13
-    SWIFTFORMAT_FORMULA="Scripts/Formulas/swiftformat.rb"
+    SWIFTFORMAT_FORMULA="swiftformat.rb"
 
     # disable homebrew auto update
     export HOMEBREW_NO_AUTO_UPDATE=1
+
+    # go to Formulas directory
+    pushd "Scripts/Formulas"
 
     # install (or reinstall) swiftlint
     install_formula swiftlint $SWIFTLINT_FORMULA
 
     # install (or reinstall) swiftformat
     install_formula swiftformat $SWIFTFORMAT_FORMULA
+
+    # pop
+    popd
 }
 
 main
