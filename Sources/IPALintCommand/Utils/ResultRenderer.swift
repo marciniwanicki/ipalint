bb8f2080632cce0dff408934acbaae77bf8a4e20
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2020-12-26 14:59:24 +0000

    Small progress

---
+++ Sources/IPALintCommand/Utils/ResultRenderer.swift
@@ -0,0 +1,8 @@
+//
+//  ResultRenderer.swift
+//  IPALintCommand
+//
+//  Created by Marcin Iwanicki on 26/12/2020.
+//
+
+import Foundation
---
+++ Sources/IPALintCore/Info/InfoResultRenderer.swift
@@ -0,0 +1,8 @@
+//
+//  InfoResultRenderer.swift
+//  IPALintCore
+//
+//  Created by Marcin Iwanicki on 26/12/2020.
+//
+
+import Foundation
