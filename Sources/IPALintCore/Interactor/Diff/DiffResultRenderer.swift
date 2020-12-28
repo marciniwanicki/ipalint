b9e21ad31b0c3bd9cb1c12e61f3d01657895fa03
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2020-12-28 00:41:21 +0000

    Add diff rendrerer

--- Sources/IPALintCore/Interactor/Diff/DiffResultRenderer.swift
+++ Sources/IPALintCore/Interactor/Diff/DiffResultRenderer.swift
@@ -15,6 +15,17 @@ public final class TextDiffResultRenderer: DiffResultRenderer {
     public init() {}
 
     public func render(result: DiffResult, to output: Output) {
-        output.write(.stdout, String(describing: result))
+        result.diff.differences.forEach { diff in
+            switch diff {
+            case let .onlyInFirst(file):
+                output.write(.stdout, "✗ \(file.path) (only in first)\n")
+            case let .onlyInSecond(file):
+                output.write(.stdout, "✗ \(file.path) (only in second)\n")
+            case let .difference(difference):
+                output.write(.stdout, "✗ \(difference.path) (different content Δ \(difference.diffSize))\n")
+                output.write(.stdout, "   1) \(difference.firstSize) (\(difference.firstSha256))\n")
+                output.write(.stdout, "   2) \(difference.secondSize) (\(difference.secondSha256))\n")
+            }
+        }
     }
 }
