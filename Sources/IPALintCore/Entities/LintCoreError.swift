f1231a0e35dac3dd59f28947ea246c0d4fd66247
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2020-12-30 23:38:19 +0000

    Improve error handling

--- Sources/IPALintCore/Entities/LintCoreError.swift
+++ Sources/IPALintCore/Entities/LintCoreError.swift
@@ -2,4 +2,29 @@ import Foundation
 
 public enum CoreError: Error {
     case generic(String)
+
+    static func rethrow<T>(_ closure: @autoclosure () throws -> T,
+                           _ message: @autoclosure () -> String) throws -> T {
+        try rethrow(closure(), .generic(message()))
+    }
+
+    static func rethrow<T>(_ closure: @autoclosure () throws -> T,
+                           _ coreError: @autoclosure () -> CoreError) throws -> T {
+        do {
+            return try closure()
+        } catch {
+            throw coreError()
+        }
+    }
+
+    static func rethrow<T>(_ closure: @autoclosure () throws -> T,
+                           _ coreError: (String) -> CoreError) throws -> T {
+        do {
+            return try closure()
+        } catch let error as LocalizedError {
+            throw coreError(error.localizedDescription)
+        } catch {
+            throw coreError(String(describing: error))
+        }
+    }
 }
